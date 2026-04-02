fc5fd0d859bc0d48fcbe1b692ecfd86aab33e375
Henri0042007 <henrique.santiago.sb@gmail.com>
2026-04-02 11:06:24 -0300

    Feat: view de cadastro #23

--- src/router/index.js
+++ src/router/index.js
@@ -34,8 +34,36 @@ const router = createRouter({
       name: "login",
       component: () => import("../views/LoginView.vue"),
       meta: { requiresAuth: false }
+    },
+    {
+      path: "/signup",
+      name: "signup",
+      component: () => import("../views/SignUpView.vue"),
+      meta: { requiresAuth: false }
+    },
+    {
+      path: "/signup/usuario",
+      name: "signup-usuario",
+      component: () => import("../components/singUp/SingUp-Usuarios.vue"),
+      meta: { requiresAuth: false }
+    },
+    {
+      path: "/signup/motorista",
+      name: "signup-motorista",
+      component: () => import("../components/singUp/SingUp-Motorista.vue"),
+      meta: { requiresAuth: false }
     }
   ],
+});
+
+const { state } = useLoginState();
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !state.logado) {
+    next({ name: "login" });
+  } else {
+    next();
+  }
 });
 
 export default router;
